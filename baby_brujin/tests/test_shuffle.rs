ba13d60becf3f9c9cd443a4d269b2fb2e8a477fe
lukas <lmuenter@gfbio.org>
2024-08-01 11:59:25 +0200

    add input stream integration tests

--- baby_brujin/tests/test_shuffle.rs
+++ baby_brujin/tests/test_shuffle.rs
@@ -17,11 +17,28 @@ fn test_shuffle_file() {
 
     let not_same_as_input = predicate::function(|x: &str| x != original_content);
 
-
     cmd.assert()
        .success()
        .stdout(predicate::str::contains("Lorem")
                .and(predicate::str::contains("ipsum"))
                .and(predicate::str::contains("dolor"))
                .and(not_same_as_input));
+}
+
+
+#[test]
+fn test_shuffle_stream_input() {
+   let input_data = "This is a test data string used for testing input streaming.";
+   let mut cmd = Command::cargo_bin("baby_brujin").unwrap();
+
+   let assert = cmd.arg("shuffle")
+      .write_stdin(input_data)
+      .assert()
+      .success();
+
+   let output_str = String::from_utf8(assert.get_output().stdout.clone()).unwrap();
+
+   let not_same_as_input = predicate::function(|x: &str| x != input_data);
+   
+   assert!(not_same_as_input.eval(&output_str), "Output should be shuffled and different from input");
 }
